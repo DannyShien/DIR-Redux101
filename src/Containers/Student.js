86c80f4680cd848cf197f8cac9f400f33b2d86e7
Stanley Chen <dannyshien@gmail.com>
2019-02-21 14:40:32 -0500

    add input field to add new name to list

--- src/Containers/Student.js
+++ src/Containers/Student.js
@@ -1,4 +1,4 @@
-import React, { component } from 'react';
+import React, { Component } from 'react';
 
 // We would like to inform this component from redux. 
 // To do that we need some glue, this container needs to 
@@ -11,7 +11,15 @@ import { connect } from 'react-redux'
 // we can get the actions to dispatch to the reducers
 import { bindActionCreators } from 'redux';
 
+import addStudent from '../Actions/addStudent';
+
 class Student extends Component {
+
+    addNewStudent = ()=>{
+        const name = document.getElementById('student-name').value;
+        this.props.addStudent(name);
+    }
+
     render() {
         const students = this.props.rightSideOfRoom.map(student => {
             return <li>{student}</li>
@@ -20,6 +28,8 @@ class Student extends Component {
         return(
             <div>
                 <h1>Students!</h1>
+                <input type="text" id="student-name" placeholder="Student Name"/>
+                <button onClick={this.addNewStudent}>Add Student</button>
                 {students}
             </div>
         )
@@ -38,7 +48,19 @@ function mapStateToProps(state) {
         rightSideOfRoom: state.students
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    // bindActionCreators takes an object as first param:
+    // property is the local prop name (this.props.aaaaa)
+    // value is the callback (the action file.js)
+    // 2nd param is the dispatcher
+    return bindActionCreators(
+        {
+            addStudent: addStudent
+        }, dispatch
+    )
+}
 console.log(connect);
 
 // export default Student:
-export default connect(mapStateToProps)(Student);
+export default connect(mapStateToProps, mapDispatchToProps)(Student);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { ETIME } from 'constants';
-import { isMainThread } from 'worker_threads';
 
 // Import the store from redux so we can use it.
 // The store, IS redux. 
@@ -15,13 +13,13 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 // We need a master/root reduver to give to the store
-import reducers from './reducers/index';
-import rootReducer from './reducer/index';
+import reducers from './Reducers/index';
+import rootReducer from './Reducers/index';
 
 // Make a store for redux to use. Pass it reducers, whish is the 
 // export of the rootreducer file. The rootreducer is just all the 
 // little reducers returns. 
-const theStore = createStore(reducer);
+const theStore = createStore(reducers);
 
 // Procider is the compnent that makes connect work (connect is inside 
 // the containers). It takes a prop of store, which is the reduxStore. 
@@ -33,4 +31,4 @@ ReactDOM.render(
         <App />
     </Provider>, 
     document.getElementById('root')
- );
+);
